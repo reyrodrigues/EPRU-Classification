577b38f65d7e1e7669d49d2b628ce014936aa5b1
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-12-29 19:21:06 -0500

    C

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
     gtx.alias('release-minor', ['bump-only:minor', 'release']);
     gtx.alias('release-major', ['bump-only:major', 'release']);
     gtx.alias('prerelease', ['bump-only:prerelease', 'release']);
-    gtx.alias('dokku:production', ['build:angular']);
+    gtx.alias('dokku:production', ['bower-install-simple', 'build:angular']);
 
     gtx.finalise();
 }
